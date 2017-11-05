c1e083400b714a018b95d0bc816869257d475979
Grant Willison <emailgdw@gmail.com>
2017-11-04 19:39:10 -0700

    URL vs NSURL breakpoint

--- cassini/DemoURL.swift
+++ cassini/DemoURL.swift
@@ -11,9 +11,9 @@ struct DemoURL {
     static let MyLinkedin = "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAeZAAAAJGVmZmY2Nzc5LWU2MzctNGQxNC04NGYxLTU3ZThhZDBlYzA3ZQ.jpg"
     
     static let AllMedia = [
-        "GPlus" : "https://lh3.googleusercontent.com/-QNeurQyo0gY/AAAAAAAAAAI/AAAAAAAACL0/IN6q4KapszI/s60-p-rw-no/photo.jpg",
-        "Cave" : "https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/21686121_10100150855501653_5562802871642660211_n.jpg?oh=ec0fe7619ef71309d623016425bb49fb&oe=5A625F83",
-        "Birthday" : "https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/22366786_10155945046874873_5478798747793880291_n.jpg?oh=cef8f5f4b603706834f74eb0c7f38eec&oe=5AA731CF",
+        "Google Plus" : "https://lh3.googleusercontent.com/-QNeurQyo0gY/AAAAAAAAAAI/AAAAAAAACL0/IN6q4KapszI/s60-p-rw-no/photo.jpg",
+        "In a Cave" : "https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/21686121_10100150855501653_5562802871642660211_n.jpg?oh=ec0fe7619ef71309d623016425bb49fb&oe=5A625F83",
+        "My Birthday" : "https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/22366786_10155945046874873_5478798747793880291_n.jpg?oh=cef8f5f4b603706834f74eb0c7f38eec&oe=5AA731CF",
         ]
     
     static func FindImageNameInList(imageName: String?) ->  URL? {
