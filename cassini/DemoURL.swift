3bac677e683b8461d84919268b397376975a9404
Grant Willison <emailgdw@gmail.com>
2017-11-09 17:31:14 -0800

    Add Multithreading and spinner

--- cassini/DemoURL.swift
+++ cassini/DemoURL.swift
@@ -11,7 +11,7 @@ struct DemoURL {
     static let MyLinkedin = "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAeZAAAAJGVmZmY2Nzc5LWU2MzctNGQxNC04NGYxLTU3ZThhZDBlYzA3ZQ.jpg"
     
     static let AllMedia = [
-        "Google Plus" : "https://lh3.googleusercontent.com/-QNeurQyo0gY/AAAAAAAAAAI/AAAAAAAACL0/IN6q4KapszI/s60-p-rw-no/photo.jpg",
+        "Google Plus" : "https://lh3.googleusercontent.com/heggIMtkGqi3X1ndFB8W-1XT-C6f9VME1Af4ZOxqZ-yQ_OB8U49N1gXLTpnltnCS0FMXxcZZy8UlsZNQSg2C-f327pc0I65kIjBIJCBY2kypdqP1lZS29aITmJiSAPp49fAr8-MdbKqXq1zXQrSzsa3RFqYOgdKq8ywqEMZfYVeL4FPlBsSAsIrxpSfDKpLqJmRbeCSXF36Kde3a4gbYbQefvO6c9uWM46Rb5uOsFoRyjykH8sPA1JVxnbI2bEwthWb5t_olOgX3Us6tQWd_WPK1lAe3BwTBidEYfUJlRXSsAbUGoUBWpapVwqZcGsl0THjDCvZJKpj22p9IIgsXqikG5T71l2f5eioDdTW80pVy96n88OlxLjTlom8BUDqve954WbisgjfqE-NS2jBKQc0pegDNvLrUb-rvJS8relX0KHxeM9Au0uWWoo7dh9yGpbRJWXH1XIQ36MkJHtBPVQ7e6w7hEkKBlum6hX6axr9ETeAGW_S15R0yQwWQzhsCiAR6f8-sg8ogi0aMG_vE5DSxhtn1-q-8FzKYeczKXItsVl0AH4aPqGms8cGQtTCZOPnDso0U03GJCu2yw6kgq61n2KLRPbEMv_0krnys1ogTdQOwRYu43InbcZWiCfY2Yuz9QltdsaDtkZA3xiXO7mFrqo_nV3bzUac=w1285-h920-no",
         "In a Cave" : "https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/21686121_10100150855501653_5562802871642660211_n.jpg?oh=ec0fe7619ef71309d623016425bb49fb&oe=5A625F83",
         "My Birthday" : "https://scontent-sjc2-1.xx.fbcdn.net/v/t1.0-9/22366786_10155945046874873_5478798747793880291_n.jpg?oh=cef8f5f4b603706834f74eb0c7f38eec&oe=5AA731CF",
         ]
--- cassini/ImageViewcontroller.swift
+++ cassini/ImageViewcontroller.swift
@@ -13,26 +13,48 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
     var imageURL: NSURL? {
         didSet{
             image = nil
-            fetchImage()
+            if view.window != nil {
+                fetchImage()
+            }
+            
         }
     }
-    
     @IBOutlet weak var scrollView: UIScrollView! {
         didSet {
             scrollView.contentSize = imageView.frame.size
             scrollView.delegate = self
-            scrollView.minimumZoomScale = 0.01 //The UIScrollView class can have a delegate that must adopt the UIScrollViewDelegate protocol. For zooming and panning to work, the delegate must implement both max and minzoomscale
-            scrollView.maximumZoomScale = 1.0
+            scrollView.minimumZoomScale = 0.1 //The UIScrollView class can have a delegate that must adopt the UIScrollViewDelegate protocol. For zooming and panning to work, the delegate must implement both max and minzoomscale
+            scrollView.maximumZoomScale = 10.0
         }
     }
+    
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    
     func viewForZooming(in scrollView: UIScrollView) -> UIView? {
         return imageView
     }
     private func fetchImage() {
         if let url = imageURL {
-            if let imageData = NSData(contentsOf: url as URL) {
-                image = UIImage(data: imageData as Data)
+            spinner?.startAnimating() // optional in case i'm fetching my image
+            DispatchQueue.global(qos: .userInitiated).async {
+                let contentsOfURL = NSData(contentsOf: url as URL)
+                DispatchQueue.main.async {
+                    if url == self.imageURL {
+                        if let imageData = contentsOfURL {
+                            self.image = UIImage(data: imageData as Data) //makes clear that this is being aptured in the heap - as long as this MVC is around - could use weak self to handle rapid pressing of buttons
+                            
+                        } else {
+                            self.spinner?.stopAnimating() // secondary catch in case of no image returned
+                        }
+                    } else {
+                        print("Ignored data returned from url: \(url)")
+                    }
+                    
+                }
+                
             }
+            
         }
     }
     private var imageView = UIImageView()
@@ -45,6 +67,7 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
             imageView.image = newValue
             imageView.sizeToFit()
             scrollView?.contentSize = imageView.frame.size
+            spinner?.stopAnimating() // if image is set spinning can stop  BUT if image data is not found we need else condition inside imageData given its not found
         }
     }
     override func viewDidLoad() {
@@ -53,7 +76,14 @@ class ImageViewController: UIViewController, UIScrollViewDelegate {
         
     }
     
-    
+    // VWappear to find out that method 'fetchImage' will now go onscreen
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if image == nil {
+            fetchImage()
+        }
+    }
+
     
     
     
